47a02202416c308c5721cdbc03540b0788c5ef08
Hetielle Vitória de Matos <hetiellematos86@gmail.com>
2023-12-25 16:01:51 -0300

    Update Main.java

--- mac/src/application/Main.java
+++ mac/src/application/Main.java
@@ -11,17 +11,18 @@ import javax.crypto.spec.PBEKeySpec;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		// gerar o Salt
-		byte[] saltBytes = Base64.getDecoder().decode("zD/lYvAi6FcChUylpBqmqQ==");//mudar
+		byte[] saltBytes = Base64.getDecoder().decode("zD/lYvAi6FcChUylpBqmqQ==");
 				
 		// gerar a chave
-		String password = "H&ti&11&Vit0ri@d&M@t0s";//mudar
-		int iterations = 65536;//conferir se é igual
-		int keySize = 512;// conferir
+		String password = "H&ti&11&Vit0ri@d&M@t0s";
+		int iterations = 65536;
+		int keySize = 512;
 		KeySpec spec = new PBEKeySpec(password.toCharArray(), saltBytes, iterations, keySize);
 		SecretKeyFactory factory;
 		try {
-			factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");//conferir 
+			factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
 			byte[] key = factory.generateSecret(spec).getEncoded();
 			String keyB64 = Base64.getEncoder().encodeToString(key);
 			System.out.print(keyB64);
